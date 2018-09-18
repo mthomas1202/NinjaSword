34a335e04b5ad849b91dc1a8eb0aecb40636f05e
Matthew Thomas <matt.thomas1202@yahoo.com>
2018-09-18 15:18:01 -0400

    endlsess path creation

---
+++ Assets/FloorManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject[] floors;
+    public float offsetLength = 40f;
+    public float playerDistance = 30f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach(GameObject floor in floors)
+        {
+            if(player.transform.position.z - floor.transform.position.z > playerDistance)
+            {
+                floor.transform.position += new Vector3(0, 0, offsetLength * floors.Length);
+            }
+        }
+	}
+}
